6d9c87932e4f97b99d730216ea385ee161721271
1Dimitri <git.dimitri@janczak.net>
2019-05-16 17:08:35 +0200

    XEvent Profiler Standard Session from SSMS 17.9.1

---
+++ XEvent_SSMS_Profiler_Standard.sql
@@ -0,0 +1,27 @@
+-- XEvent Profiler Standard Session 
+-- from SSMS 17.9.1
+CREATE EVENT SESSION [XES_SSMS_Profiler_Standard] ON SERVER 
+ADD EVENT sqlserver.attention(
+    ACTION(package0.event_sequence,sqlserver.client_app_name,sqlserver.client_pid,sqlserver.database_id,sqlserver.database_name,sqlserver.nt_username,sqlserver.query_hash,sqlserver.server_principal_name,sqlserver.session_id)
+    WHERE ([package0].[equal_boolean]([sqlserver].[is_system],(0)))),
+ADD EVENT sqlserver.existing_connection(SET collect_options_text=(1)
+    ACTION(package0.event_sequence,sqlserver.client_app_name,sqlserver.client_hostname,sqlserver.client_pid,sqlserver.nt_username,sqlserver.server_principal_name,sqlserver.session_id)),
+ADD EVENT sqlserver.login(SET collect_options_text=(1)
+    ACTION(package0.event_sequence,sqlserver.client_app_name,sqlserver.client_hostname,sqlserver.client_pid,sqlserver.nt_username,sqlserver.server_principal_name,sqlserver.session_id)),
+ADD EVENT sqlserver.logout(
+    ACTION(package0.event_sequence,sqlserver.client_app_name,sqlserver.client_pid,sqlserver.nt_username,sqlserver.server_principal_name,sqlserver.session_id)),
+ADD EVENT sqlserver.rpc_completed(
+    ACTION(package0.event_sequence,sqlserver.client_app_name,sqlserver.client_pid,sqlserver.database_id,sqlserver.database_name,sqlserver.nt_username,sqlserver.query_hash,sqlserver.server_principal_name,sqlserver.session_id)
+    WHERE ([package0].[equal_boolean]([sqlserver].[is_system],(0)))),
+ADD EVENT sqlserver.sql_batch_completed(
+    ACTION(package0.event_sequence,sqlserver.client_app_name,sqlserver.client_pid,sqlserver.database_id,sqlserver.database_name,sqlserver.nt_username,sqlserver.query_hash,sqlserver.server_principal_name,sqlserver.session_id)
+    WHERE ([package0].[equal_boolean]([sqlserver].[is_system],(0)))),
+ADD EVENT sqlserver.sql_batch_starting(
+    ACTION(package0.event_sequence,sqlserver.client_app_name,sqlserver.client_pid,sqlserver.database_id,sqlserver.database_name,sqlserver.nt_username,sqlserver.query_hash,sqlserver.server_principal_name,sqlserver.session_id)
+    WHERE ([package0].[equal_boolean]([sqlserver].[is_system],(0))))
+-- addition of a 10x100MB set of files to record data, add full path if you don't want the file go into the Logs SQL Directory
+ADD TARGET package0.event_file(SET filename=N'XES_SSMS_Profiler_Standard',max_file_size=(100),max_rollover_files=(10))
+WITH (MAX_MEMORY=8192 KB,EVENT_RETENTION_MODE=ALLOW_SINGLE_EVENT_LOSS,MAX_DISPATCH_LATENCY=5 SECONDS,MAX_EVENT_SIZE=0 KB,MEMORY_PARTITION_MODE=PER_CPU,TRACK_CAUSALITY=ON,STARTUP_STATE=OFF)
+GO
+
+
