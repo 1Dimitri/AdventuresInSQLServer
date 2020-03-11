26eaf6d9ecfed7da18eb1c45d11f3d5d349f4678
1Dimitri <git.dimitri@janczak.net>
2020-03-11 12:11:18 +0100

    Create Check_Powershell_Version.sql

---
+++ Inventory/Check_Powershell_Version.sql
@@ -0,0 +1,85 @@
+-- Check the powershell version launched by a SQL Server agent
+
+USE [msdb]
+GO
+
+BEGIN TRANSACTION
+DECLARE @ReturnCode INT
+SELECT @ReturnCode = 0
+IF NOT EXISTS (SELECT name FROM msdb.dbo.syscategories WHERE name=N'[Inventory]' AND category_class=1)
+BEGIN
+EXEC @ReturnCode = msdb.dbo.sp_add_category @class=N'JOB', @type=N'LOCAL', @name=N'[Inventory]'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+
+END
+
+DECLARE @jobId BINARY(16)
+EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'Test SQLPS Version', 
+		@enabled=1, 
+		@notify_level_eventlog=0, 
+		@notify_level_email=0, 
+		@notify_level_netsend=0, 
+		@notify_level_page=0, 
+		@delete_level=0, 
+		@description=N'No description available.', 
+		@category_name=N'[Uncategorized (Local)]', 
+		@owner_login_name=N'CIBAT\jq467', @job_id = @jobId OUTPUT
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'$PSVersionTable', 
+		@step_id=1, 
+		@cmdexec_success_code=0, 
+		@on_success_action=3, 
+		@on_success_step_id=0, 
+		@on_fail_action=2, 
+		@on_fail_step_id=0, 
+		@retry_attempts=0, 
+		@retry_interval=0, 
+		@os_run_priority=0, @subsystem=N'PowerShell', 
+		@command=N'($PSVersionTable | ConvertTo-Xml -NoTypeInformation).OuterXml', 
+		@database_name=N'master', 
+		@flags=8
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Convert Back to XML Object', 
+		@step_id=2, 
+		@cmdexec_success_code=0, 
+		@on_success_action=1, 
+		@on_success_step_id=0, 
+		@on_fail_action=2, 
+		@on_fail_step_id=0, 
+		@retry_attempts=0, 
+		@retry_interval=0, 
+		@os_run_priority=0, @subsystem=N'TSQL', 
+		@command=N'SET QUOTED_IDENTIFIER ON
+
+SELECT
+	[object].value(''Property[@Name="Value"][1]'',''nvarchar(48)'') PSVersion,
+	[object].value(''Property[@Name="Value"][2]'',''nvarchar(48)'') PSEdition
+
+INTO tempdb.dbo.SQLPSVersion
+FROM (
+SELECT TOP(1) CONVERT(XML,CONVERT(VARCHAR(MAX),[log])) AS XmlData
+from msdb.dbo.sysjobstepslogs 
+where step_uid in (
+select    step_uid 
+from msdb.dbo.sysjobsteps
+where job_id=  CONVERT(uniqueidentifier,$(ESCAPE_NONE(JOBID)))
+)
+ORDER BY date_modified DESC
+) AS dummy
+CROSS APPLY XmlData.nodes(''//Object'') as PSObject([object])
+', 
+		@database_name=N'master', 
+		@flags=0
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_update_job @job_id = @jobId, @start_step_id = 1
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_add_jobserver @job_id = @jobId, @server_name = N'(local)'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+COMMIT TRANSACTION
+GOTO EndSave
+QuitWithRollback:
+    IF (@@TRANCOUNT > 0) ROLLBACK TRANSACTION
+EndSave:
+GO
+
+
